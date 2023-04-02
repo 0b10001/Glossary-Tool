299081551a10735fbfa4c1d979abeebfa88efd8e
MphoM242 <126402566+MphoM242@users.noreply.github.com>
2023-04-02 07:48:01 +0200

    Updated doubleClick.js (will use after Sprint1)
    Removed extra/ unnecessary code and modified to what I think is useful.
    NOTE: Not priority for Sprint 1, but will probably be required by end of Sprint 2.

--- doubleClick.js
+++ doubleClick.js
@@ -1,28 +1,11 @@
-
-//window.onload= setupExtDoubleClick( 'http://dictionary.cambridge.org/', 'british', false, null, 5, 'popup' ) 
-
-/*$(document).ready(function() {
-    $("#definition button").click(function() {
-        $(this).parent().hide(200);
-    });
-    $("#container p").dblclick(function() {
-        var txt = getSelectedText();
-        //$.get("/php/data/define.php", { q: txt }, function(data) { 
-            var $dataDiv = $("<div>"+data+"</div>").addClass("data");
-            $("#definition").show(200)
-                .contents("h3").text("Definition of: "+txt).parent()
-                .contents("div.data").replaceWith($dataDiv).parent();
-       // });
-    });
-});
-This one can be used when the HTML file has the elements followed by the hashes  */ 
-
+//Not completed. (Open pop-up method)
+//might need to create modal to use this code with. Don't think toggletip will work. 
 
 //will tell which word was seleted, pop-up
 $(document).dblclick(function(e) {
     var t = getSelectedText();
     /*window.onload=function(){
-       openPopup();
+       //openPopup(); 
     }
     function openPopup(){
         $()

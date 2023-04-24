5022a8706865495ad0cde33b5457efe2de86bdc7
0b10001 <ob1001ob1001@gmail.com>
2023-04-24 04:10:01 +0000

    added unit test for background

--- frontend/dist/background.test.js
+++ frontend/dist/background.test.js
@@ -1,4 +1,18 @@
 describe('background.js', () => {
+  beforeEach(() => {
+    global.chrome = {
+      runtime: {
+        onMessage: {
+          addListener: jest.fn(),
+        },
+      },
+    };
+  });
+
+  afterEach(() => {
+    delete global.chrome;
+  });
+
   it('should load without errors', () => {
     expect(() => {
       require('../src/background.js');

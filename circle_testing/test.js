471a7d046f44d10221e1a79c0fbb0df70820108b
0b10001 <ob1001ob1001@gmail.com>
2023-04-25 10:28:42 +0200

    jest

--- circle_testing/test.js
+++ circle_testing/test.js
@@ -1,12 +1,9 @@
 const { backgroundRunning, receiver } = require('./src');
 
 // Mock the chrome.runtime.onMessage.addListener method
-jest.mock('chrome', () => ({
-  runtime: {
-    onMessage: {
-      addListener: jest.fn()
-    }
-  }
+jest.mock('./src', () => ({
+  backgroundRunning: jest.fn(),
+  receiver: jest.fn()
 }));
 
 // Unit test for backgroundRunning function

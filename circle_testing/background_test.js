e9d29966f34b2f1b9fd24d034e5531aa719fceb2
0b10001 <ob1001ob1001@gmail.com>
2023-04-25 10:17:03 +0200

    back_test

---
+++ circle_testing/background_test.js
@@ -0,0 +1,27 @@
+//Notification of the background running
+function backgroundRunning() {
+  console.log('background running');
+}
+
+//Listen for a message from the content.js and adding an event listener to the message
+chrome.runtime.onMessage.addListener(receiver);
+
+//Word in the background
+window.word = "Glossary Tool";
+
+//when message is received do this
+function receiver(request, sender, sendResponse){
+    //show what we are receiving
+    console.log(request);
+
+    //Set the new background word to be the receieved word
+    window.word = request.text;
+
+}
+
+//Export functions for testing
+module.exports = {
+  backgroundRunning,
+  receiver
+};
+

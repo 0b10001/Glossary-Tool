f1ad57fa72da7b2c33552d4b4d311e272be2d856
MphoM242 <126402566+MphoM242@users.noreply.github.com>
2023-05-14 15:38:28 +0200

    Signin added to be bundled

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
     contentg: './src/contentg.js',
     background: './src/background.js',
     main: './src/GT.js',
+    signin: './src/signin.js'
   },
   output: {
     filename:'[name].bundle.js',

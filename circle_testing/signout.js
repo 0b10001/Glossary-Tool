37ffdf916187751c0bd56aee99d7572cc5496cff
Puseletso931 <127834075+Puseletso931@users.noreply.github.com>
2023-05-18 10:21:16 +0200

    Delete signout.js

--- circle_testing/signout.js
+++
@@ -1,8 +0,0 @@
-function signOut(){
-    //remove the data
-    localStorage.removeItem('signedIn');
-    localStorage.removeItem('email');
-    localStorage.removeItem('dateTime');
-    alert("You are signed out");
-  }
-  module.exports = { signOut };

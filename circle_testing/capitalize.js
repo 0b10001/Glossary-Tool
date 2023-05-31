8e5b6a41ffb4f62a6e167de2cf7cabe5a80e1b4e
Q1q2q3a1 <106957148+Q1q2q3a1@users.noreply.github.com>
2023-05-31 20:41:32 +0200

    Delete capitalize.js

--- circle_testing/capitalize.js
+++
@@ -1,9 +0,0 @@
-function capitalize(str) {
-  if (typeof str !== 'string') {
-    throw new TypeError('Expected a string');
-  }
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-module.exports = capitalize;
-
